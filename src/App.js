fd1eea0a6bf15294b454b8075cd13844a3d1ca34
Ella R <ryukhinaella@gmail.com>
2023-02-07 21:35:36 -0800

    Form is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import SearchMovies from './searchMovie';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
       <h1 className="title">React Movie Search</h1>
+      <SearchMovies />
     </div>
   );
 }
